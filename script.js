e3a9e1d6fde37b01e8aa9665f12d18b118cdb392
rubensalves64 <rubens.pita2000@gmail.com>
2024-04-06 20:42:46 +0100

    incializando o projeto

---
+++ script.js
@@ -0,0 +1,27 @@
+const participante = {
+  nome: "Rubens",
+  email: "rubens@gmail.com",
+  dataInscriçao: new Date(2024, 2, 22, 19, 20),
+  dataInscriçao: new Date(2024, 2, 25, 19, 20),
+};
+
+let participante = n;
+
+const criarNovoParticipante = (participante) => {
+  return `
+    <tr>
+  <td>
+    <strong>${participante.nome}</strong>
+    <br />
+    <small>${participante.email}</small>
+  </td>
+  <td>${participante.dataInscriçao}</td>
+  <td>${participante.dataInscriçao}</td>
+</tr>`;
+};
+
+const atualizarlista = (participante) => {
+  document.querySelector("tbody").innerHTML =
+    criarNovoParticipante(participante);
+};
+atualizarlista();
